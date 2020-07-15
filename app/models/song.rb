c646cf55b663a8147ddc4be89daa11eebbaef236
Ginger-Mano <mcasimirian@gmail.com>
2020-07-15 17:28:36 -0400

    third

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
     has_many :song_genres
     has_many :genres, through: :song_genres
     belongs_to :artist
+
+    def slug
+        self.name.split.join("-").downcase
+    end
+
+    def self.find_by_slug(slug)
+        downcase_song_name = slug.split("-").join(" ")
+        self.all.find { |song| song.name.downcase == downcase_song_name }
+    end
 end
